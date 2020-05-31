8699952272ad005fb4fe5b4f27e6743011f69d30
CribberSix <seifert.victor@web.de>
2020-05-31 15:04:42 +0200

    added how-to-use.py

---
+++ how-to-use.py
@@ -0,0 +1,23 @@
+import pygame
+from pygametexteditor import TextEditor
+
+pygame.init()
+systemHeight = 600
+systemWidth = 900
+screen = pygame.display.set_mode((systemWidth, systemHeight))
+pygame.display.set_caption("TextEditor Setup")
+pygame.display.get_surface().fill((200, 200, 200))  # background
+
+# TextEditor setup + parameters
+myscreen = pygame.display.get_surface()
+offset_X = 50
+offset_Y = 50
+codingAreaHeight = 500
+codingAreaWidth = 800
+displayLineNumbers = True
+TX = TextEditor(offset_X, offset_Y, codingAreaWidth, codingAreaHeight, myscreen, displayLineNumbers)
+
+# Editor loop
+while True:
+    TX.display_CodeEditor()  # has to be called once per loop to capture input
+    pygame.display.flip()
