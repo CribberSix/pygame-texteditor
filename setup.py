571d472a913511ecaa8f53681b6a3a920826a9d9
CribberSix <seifert.victor@web.de>
2020-05-31 10:47:57 +0200

    c

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pygame-texteditor',
+    packages=['pygame-texteditor'],
+    description='A simple WYSIWYG-texteditor based on pygame.',
+    version='0.1',
+    url='http://github.com/example/linode_example',
+    author='Linode',
+    author_email='docs@linode.com',
+    keywords=['pip','linode','example']
+    )
