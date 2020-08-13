728f9a8994dddd5a0d3e7850ed2252b0e3a8597c
CribberSix <seifert.victor@web.de>
2020-08-13 08:27:07 +0200

    setup

--- setup.py
+++ setup.py
@@ -14,22 +14,24 @@ setup(
     long_description_content_type='text/markdown',
     version='0.0.45',
     python_requires=">=3.5",
-    url='https://github.com/CribberSix/pygame-texteditor',
+    #url='https://github.com/CribberSix/pygametexteditor', # private
     author='CribberSix',
     author_email='cribbersix@gmail.com',
     install_requires=['pygame', 'math', 'os', 'sys'],
     classifiers=[
         # Trove classifiers
         # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Topic :: Software Development',
     ],
-    keywords=['pygame','texteditor'],
+    keywords=['pygame', 'texteditor', 'text', 'editor'],
     package_data={'pygametexteditor': ['pygametexteditor/elements/*.png', 'pygametexteditor/elements/*.ttf']},
     include_package_data=True,
 )
