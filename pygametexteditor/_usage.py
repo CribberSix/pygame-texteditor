3b40f0e15a16760e4676593e5a8658b485783973
CribberSix <seifert.victor@web.de>
2020-08-28 10:48:39 +0200

    added get_text_as_array func

--- pygametexteditor/_usage.py
+++ pygametexteditor/_usage.py
@@ -1,6 +1,9 @@
 import pygame
 
-def get_text(self):
+
+def get_text_as_string(self):
     return "\n".join(self.line_String_array)
 
 
+def get_text_as_array(self):
+    return self.line_String_array
